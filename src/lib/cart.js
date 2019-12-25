7b5ece4f1449a8ab5675a664a9e0401ce42db4ac
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-25 18:23:32 -0400

    02 Adicionando Itens ao Carrinho

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -16,9 +16,75 @@ const Cart = {
 
         return this
     },
-    addOne(product){},
+    addOne(product){
+        // ver se o produto já existe no carrinho
+        let inCart = this.items.find(item => item.product.id == product.id)
+
+        // se não exite
+        if (!inCart) {
+            inCart = {
+                product: {
+                    ...product,
+                    formattedPrice: formatPrice(product.price)
+                },
+                quantity: 0,
+                price: 0,
+                formattedPrice: formatPrice(0)
+            }
+
+            this.items.push(inCart)
+        }
+
+        // max quantity exceed
+        if(inCart.quantity >= product.quantity) return this
+
+        // update item
+        inCart.quantity++
+        inCart.price = inCart.product.price * inCart.quantity
+        inCart.formattedPrice = formatPrice(inCart.price)
+
+
+        // update cart
+        this.total.quantity++
+        this.total.price += inCart.product.price
+        this.total.formattedPrice = formatPrice(this.total.price)
+
+        return this
+    },
     removeOne(productId){},
     delete(productId){}
 }
+
+const product = {
+    id: 1,
+    price: 199,
+    quantity: 2
+}
+
+
+const product2 = {
+    id: 2,
+    price: 229,
+    quantity: 1
+}
+
+console.log('add first cart item')
+let oldCart = Cart.init().addOne(product)
+console.log(oldCart)
+
+console.log('add second cart item')
+oldCart = Cart.init(oldCart).addOne(product)
+console.log(oldCart)
+
+console.log('add third cart item')
+oldCart = Cart.init(oldCart).addOne(product2)
+console.log(oldCart)
+
+
+console.log('add last cart item')
+oldCart = Cart.init(oldCart).addOne(product)
+console.log(oldCart)
+
+
 
 module.exports = Cart
