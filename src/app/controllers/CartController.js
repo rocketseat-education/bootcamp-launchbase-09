32dbd8d9cc2be99e5741bbc5f27227a9d5decd9a
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-26 08:09:26 -0400

    07 Utilizando Funcionalidade de Adicionar ao Cart

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -5,19 +5,35 @@ const LoadProductsService = require('../services/LoadProductService')
 module.exports = { 
     async index(req, res) {
         try {  
-
-            const product = await LoadProductsService.load('product', {where: { id: 1}})
-
             let { cart } = req.session
 
             // gerenciador de carrinho
-            cart = Cart.init(cart).addOne(product)
+            cart = Cart.init(cart)
 
             return res.render("cart/index", { cart })
         }
         catch(err) {
             console.error(err)
         }
+
+    },
+    async addOne(req,res) {
+        // pegar o id do produto e o produto
+        const { id } = req.params
+
+        const product = await LoadProductsService.load('product', {where: { id }})
+
+        // pegar o carrinho da sessão
+        let { cart } = req.session
+
+        // adicionar o produto ao carrinho (usando nosso gerenciador de carrinho)
+        cart = Cart.init(cart).addOne(product)
+
+        // atualizar o carrinho da sessão
+        req.session.cart = cart
+
+        // redirecionar o usuário para a tela do carrinho.
+        return res.redirect('/cart')
 
     }
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,5 +4,6 @@ const routes = express.Router()
 const CartController = require('../app/controllers/CartController')
 
 routes.get('/', CartController.index)
+    .post('/:id/add-one', CartController.addOne)
 
 module.exports = routes
