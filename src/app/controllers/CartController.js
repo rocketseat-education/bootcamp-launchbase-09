20e0c04501a1e03f2edb385404c412b5e7a80f1c
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-26 09:48:58 -0400

    09 Cart Empty

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -55,5 +55,15 @@ module.exports = {
 
         // redirecionamento para a pagina cart
         return res.redirect('/cart')
+    },
+    delete(req, res) {
+        let { id } = req.params
+        let { cart } = req.session
+
+        if(!cart) return
+
+        req.session.cart = Cart.init(cart).delete(id)
+
+        return res.redirect('/cart')
     }
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -6,5 +6,6 @@ const CartController = require('../app/controllers/CartController')
 routes.get('/', CartController.index)
     .post('/:id/add-one', CartController.addOne)
     .post('/:id/remove-one', CartController.removeOne)
+    .post('/:id/delete', CartController.delete)
 
 module.exports = routes
