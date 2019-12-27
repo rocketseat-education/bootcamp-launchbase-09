7ec232d7d0ebf477632cb6bbdac7f77c337634d9
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-27 16:39:22 -0400

    17 Minhas Vendas e Detalhes do Pedido

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -8,5 +8,6 @@ const { onlyUsers } = require('../app/middlewares/session')
 routes.post('/', onlyUsers, OrderController.post)
     .get('/', onlyUsers, OrderController.index)
     .get('/sales', onlyUsers, OrderController.sales)
+    .get('/:id', onlyUsers, OrderController.show)
 
 module.exports = routes
