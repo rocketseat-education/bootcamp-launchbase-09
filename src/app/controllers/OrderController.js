542cbea159082e94200e7d170336bdb3cf4dc716
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-29 12:13:19 -0400

    21 Mudar Status do Pedido

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -113,5 +113,43 @@ module.exports = {
             return res.render('orders/error')
         }
 
+    },
+    async update(req, res) {
+        try {
+
+            const { id, action } = req.params
+
+            const acceptedActions = ['close', 'cancel']
+            
+            if(!acceptedActions.includes(action)) return res.send("Can't do this action")
+
+            // pegar o pedido
+            const order = await Order.findOne({
+                where: { id }
+            })
+
+            if(!order) return res.send('Order not found')
+
+            // verificar se ele está aberto
+            if(order.status != 'open') return res.send("Can't do this action")
+
+            // atualizar o pedido
+            const statuses = {
+                close: "sold",
+                cancel: "canceled"
+            }
+
+            order.status = statuses[action]
+
+            await Order.update(id, {
+                status:  order.status
+            })
+
+            // redirecionar
+            return res.redirect('/orders/sales')
+            
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -9,5 +9,6 @@ routes.post('/', onlyUsers, OrderController.post)
     .get('/', onlyUsers, OrderController.index)
     .get('/sales', onlyUsers, OrderController.sales)
     .get('/:id', onlyUsers, OrderController.show)
+    .post('/:id/:action', onlyUsers, OrderController.update)
 
 module.exports = routes
